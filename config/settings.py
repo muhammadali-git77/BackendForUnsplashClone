f936b9ae6022d422f64a6d03de1fa9feeac6403c
Muhammadali Abdumannonov <aalixon636@gmail.com>
2025-03-24 21:06:37 +0500

    SQL to'g'irlandi

--- config/settings.py
+++ config/settings.py
@@ -147,19 +147,19 @@ AUTHENTICATION_BACKENDS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'BGXWzeqekNSCnQCDmhpcVVTOIOTyDSsG',
+        'HOST': 'shuttle.proxy.rlwy.net',
+        'PORT': '17525',
     }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
